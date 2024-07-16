fb6f559bf66610fa5e153360cc7d02ef35cda06a
thedemonsid <siddheshshrirame@gmail.com>
2024-07-16 18:58:20 +0530

    init

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter } from "next/font/google";
+import { Lusitana } from "next/font/google";
+import { Playwrite_AU_NSW } from "next/font/google";
+export const playwrite = Playwrite_AU_NSW({ weight: "400" });
+export const lusitana = Lusitana({ subsets: ["latin"], weight: "400" });
+export const inter = Inter({ subsets: ["latin"] });
